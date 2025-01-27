2257ec249bdd9a20a2a8396279366f08d1f22125
Don Syme <donsyme@fastmail.fm>
2025-01-27 21:22:46 +0000

    changes names

--- WebJobContinuous/run.sh
+++ WebJobContinuous/run.sh
@@ -4,6 +4,7 @@
 while true; do
   echo "Continuous WebJob Current time: $(date)"
   echo "DOCKER_REGISTRY_SERVER_URL: $DOCKER_REGISTRY_SERVER_URL"
+  echo "TEST_ENV: $TEST_ENV"
   sleep 1
 done
 #dotnet ./WebJobContinuous.dll
