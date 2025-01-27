dde320bea41caca269e40db2e41bbaa8a75d4d45
Don Syme <donsyme@fastmail.fm>
2025-01-27 19:21:09 +0000

    changes names

--- dockerfile-supporting-files/entrypoint.sh
+++ dockerfile-supporting-files/entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 # Following instructions from
 # <https://learn.microsoft.com/en-us/azure/app-service/configure-custom-container?tabs=debian&pivots=container-linux#enable-ssh>
 service ssh start
-# Add an empty string as the GHCS_NPM_TOKEN to prevent startup failures due to failing env var replacement in .npmrc
 dotnet WebApi.dll
